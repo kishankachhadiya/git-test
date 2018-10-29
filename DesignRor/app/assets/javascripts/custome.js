ab563092382757be86f9386a5775c4cd98ce7d2d
kishan.sutex.kp@gmail.com <K|$#@n_2906>
2018-10-29 18:08:29 +0530

    initial commit

---
+++ DesignRor/app/assets/javascripts/custome.js
@@ -0,0 +1,12 @@
+// Action Cable provides the framework to deal with WebSockets in Rails.
+// You can generate new channels where WebSocket features live using the `rails generate channel` command.
+//
+//= require action_cable
+//= require_self
+//= require_tree ./channels
+
+$(function(){
+    $(".fold-table tr.view").on("click", function(){
+        $(this).toggleClass("open").next(".fold").toggleClass("open");
+    });
+});
